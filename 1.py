c166c5d90671dd649992b92575fec4e7871da20d
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-16 19:51:03 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,96 @@
+#practise pyuthon challenge 50 days 
+#day=1 practise
+
+name=("manjunatrh gowda")
+print(name)
+
+age=18
+city="hassan"
+print(f"My age is{age} and city is {city}")
+
+x=10
+print(type(x))
+
+college="navkis"
+print(college)
+
+a=5
+b=10
+sum=a+b
+print(sum)
+
+is_student=True
+print(is_student)
+
+name="manjunath"
+age=18
+branch="cse"
+print(f"My name is {name} and age  is {age} and branch is {branch}")
+
+a=10
+b=2.5
+c="manju"
+d=True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+num=5,4,6,3,4,65
+print(num)
+
+city=("bangaloure","Hassan","mandya")
+print(city)
+
+a=10
+b=2.5
+c="manju"
+d=True
+print("integer:",a)
+print("floating:",b)
+print("string:",c)
+print("boolean:",d)
+
+a=10
+b=2.5
+c="manju"
+d=True
+print(float(a))
+print(int(b))
+print(bool(c))
+print(str(a))
+
+num=input("enter your number:-")
+print(int(num))
+
+a=input("enter your first number:")
+b=input("enter your second number:")
+sum=a+b
+print(int(sum))
+
+x,y,z=10,3,5
+print(x,y,z)
+
+a=b=c=100
+print(a,b,c)
+
+a=5
+b=10
+
+a,b=b,a
+temp=a
+a=b
+print("b:",temp)
+print(a)
+
+x=5
+print(x)
+x=10
+print(x)
+
+count=10
+i=count+1
+print(i)
+
+
+
