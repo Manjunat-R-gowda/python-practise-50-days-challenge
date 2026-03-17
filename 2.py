c9e73ebf64979d3f82b46c1331e88862635191f2
Manjunath Gowda <manjunathgowda2514@gmail.com>
2026-03-17 19:51:13 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,40 @@
+#practise python day=2
+
+x=10
+x+=5
+print(x)
+
+
+div=int(input("enter a number:-"))
+div /=2
+print(div)
+
+a=10
+b=5
+a,b=b,a
+print(a)
+print(b)
+
+for i  in range(1,6):
+    i+=1
+    print(i)
+
+exp=int(input("enter your expensive:-"))
+total=0
+for i in range(1,exp+1):
+    total+=i
+print(i)
+
+
+salary=10
+salary+=10
+print(salary)
+
+a=5
+b=5
+print(a*b)
+
+
+a=5
+b=10
+print(a&b)
